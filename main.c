4d346f08f0c1750a439b65fdfdb0a6e00c18cccb
Maulaz Tony <tony.maulaz@heig-vd.ch>
2021-02-05 12:52:05 +0100

    Create program main.c

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
-int main(){}
+#include <stdlib.h>
+#include <math.h>
+
+int func(int* tab){
+    tab[0] = 12;
+    tab[4] = -100;
+    tab[1] = 0;
+}
+
+int main(int argc, char* argv[]){
+    int tab[] = {1,3,5,6,2,3,4,5,6};
+    const size_t size = sizeof(tab) / sizeof(int);
+
+    func(tab);
+
+    double mean = 0.;
+    for(size_t i = 0; i<size; i++){
+        mean = 0.1*tab[i] + 0.9*mean;
+    }
+
+    for(int i = 0; i<1000; i++){
+        double val = exp( tab[(i+3)%size] ) * pow(i,4);
+    }
+}
